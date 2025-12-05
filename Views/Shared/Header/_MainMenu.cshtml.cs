e6b33754d01bc6f683ce4ce5d167c2d9b7c8ce50
Alexei Rotari <alex21.it.dd@gmail.com>
2025-12-05 16:50:02 +0100

    add header menu

---
+++ Views/Shared/Header/_MainMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace News.Views.Shared.Header
+{
+    public class _MainMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
