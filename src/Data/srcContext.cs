3f887e386519821415584d944f8770b075e3615a
Alexei Rotari <alex21.it.dd@gmail.com>
2025-12-21 23:52:42 +0100

    add project, controller, migration and tests

---
+++ src/Data/srcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using src.Models;
+
+namespace src.Data
+{
+    public class srcContext : DbContext
+    {
+        public srcContext (DbContextOptions<srcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<src.Models.Article> Article { get; set; } = default!;
+    }
+}
